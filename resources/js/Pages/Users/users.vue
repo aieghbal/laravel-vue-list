ac9d1f72ed3a260ea9895c6a66a61f3468270556
AmirEghbal-me <aieghbal.me@gmail.com>
2024-05-06 14:52:48 +0330

    first commit

---
+++ resources/js/Pages/Users/users.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>User List</h1>
+        <ul>
+            <li v-for="user in users" :key="user.id">{{ user.name }}</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        users: Array,
+    },
+}
+</script>
